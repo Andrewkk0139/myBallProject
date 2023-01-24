f22ed3aa5ef75d8c275bb9f1b026a00bdf6d0d99
ANDREW KAISER <122410871+Andrewkk0139@users.noreply.github.com>
2023-01-24 15:40:16 +0000

    added a comment on line 7.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main
 {
     public static void main(String[] args)
     {
+      System.out.println("Made a comment in replit");
 
         JFrame myFrame = new JFrame();
         myFrame.setSize(500,500);
